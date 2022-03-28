9ecfad76383db627a215db89fa39f684e443fd21
SirJul1337 <niklas.jul@julit.dk>
2022-03-28 10:41:54 +0200

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Geometri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nSquare 1");
+            Square sq1 = new Square(10);
+            Console.WriteLine("Circumference: " + sq1.Circumference());
+            Console.WriteLine("Areal: " + sq1.Area());
+
+            Console.WriteLine("\nSquare 2");
+            Square sq2 = new Square(8);
+            Console.WriteLine("Circumference: " + sq2.Circumference());
+            Console.WriteLine("Areal: " + sq2.Area());
+
+            Console.WriteLine("\nSquare 3");
+            Square sq3 = new Square(7);
+            Console.WriteLine("Circumference: "+sq3.Circumference());
+            Console.WriteLine("Areal: "+sq3.Area());
+
+
+            // Assignment B
+           Square sq4 = new Square();
+            sq4.SideA = 5;
+            Console.WriteLine("\nSquare 4");
+            Console.WriteLine("Side A: ");
+            Console.WriteLine(sq4.SideA);
+            Console.WriteLine("Circumference: ");
+            Console.WriteLine(sq4.Circumference());
+            Console.WriteLine("Area: ");
+            Console.WriteLine(sq4.Area());
+            
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private int sideA;
+
+        public Square()
+        {
+
+        }
+        public Square(int sideA)
+        {
+            this.sideA = sideA;
+        }
+        public int Circumference()
+        {
+            return (sideA * 4);
+        }
+        public int Area()
+        {
+            return (SideA * SideA);
+        }
+
+        public int SideA
+        {
+            get { return sideA; }
+            set { sideA = value; }
+        }
+
+    }
+}
